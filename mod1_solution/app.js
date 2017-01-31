67df3900874dc40c7b0c3a0fcdfd7a83bc1bf254
Tobias Lundell <toblu611@student.liu.se>
2017-01-31 19:26:34 +0100

    Finished! All functionality added

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -9,29 +9,33 @@
     $scope.dishes = "";
     $scope.totalNoOfDishes = 0;
     $scope.message = "";
+    $scope.colour = "";
 
+    /**
+    Displays message to the user. Does not count empty elements.
+    */
     $scope.displayMessage = function () {
-      if ($scope.dishes != "") {
-        $scope.totalNoOfDishes = countDishes($scope.dishes);
-        if (isTooMany($scope.totalNoOfDishes) == true) {
-          $scope.message="Too much!";
-        } else {
-          $scope.message="Enjoy!";
-        }
-      }
-      else {
-        $scope.message="Please enter data first"
+      var dishes = $scope.dishes.replace(/\s/g, ''); //Remove all white spaces
+      $scope.totalNoOfDishes = countDishes(dishes); //Count dishes
+      if (dishes == "") {
+        $scope.message = "Please enter data first"; //Nothing but whitespace inputed
+        $scope.colour = "red"; //Set font and border colour to red
+      } else if ($scope.totalNoOfDishes < 4) {
+        $scope.message = "Enjoy!"; //3 or less dishes
+        $scope.colour = "green"; // Set font and border colour to green
+      } else {
+        $scope.message = "Too much!"; //4 or more dishes
+        $scope.colour = "red"; //Set font and border colour to red
       }
     };
 
+    /**
+    Counts the number of dishes. Does not consider empty elements to the count.
+    */
     function countDishes(dishes) {
-      var totalNoOfDishes = dishes.split(',').length;
-      return totalNoOfDishes;
+      var totalNoOfDishes = dishes.split(',').filter(String).length; //Split comma-separated values, and filter out empty elements
+      return totalNoOfDishes; // Number of non-empty elements
     }
-
-    function isTooMany(noOfDishes) {
-      return noOfDishes > 3;
-    };
   }
 
 })();
